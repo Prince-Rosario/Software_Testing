12763dfdad6e06b2264d111565bad2a0063412a3
Prince Rosario <82267672+Prince-Rosario@users.noreply.github.com>
2024-06-17 14:53:42 +0000

    add ui test cases

--- ui_tests/app.py
+++ ui_tests/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, render_template
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from solution import Solution
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates')
 
 @app.route('/', methods=['GET', 'POST'])
 def min_window_form():
--- ui_tests/test_e2e.py
+++ ui_tests/test_e2e.py
@@ -1,24 +1,62 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 class MinWindowE2ETest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
+        self.driver.get("http://localhost:5000")
 
-    def test_min_window(self):
+    def test_empty_string_t(self):
+        self._submit_form("anystring", "")
+        self._verify_result("")
+
+    def test_single_character_match(self):
+        self._submit_form("a", "a")
+        self._verify_result("a")
+
+    def test_single_character_no_match(self):
+        self._submit_form("a", "b")
+        self._verify_result("")
+
+    def test_no_substring_found(self):
+        self._submit_form("abcdef", "xyz")
+        self._verify_result("")
+
+    def test_large_input(self):
+        self._submit_form("a" * 100000, "aa")
+        self._verify_result("aa")
+
+    def test_t_characters_not_in_s(self):
+        self._submit_form("abc", "xyz")
+        self._verify_result("")
+
+    def test_s_and_t_same(self):
+        self._submit_form("exact", "exact")
+        self._verify_result("exact")
+
+    def test_all_characters_in_s_needed_for_t(self):
+        self._submit_form("ab", "ab")
+        self._verify_result("ab")
+
+    def test_s_contains_repeated_sequences_of_t(self):
+        self._submit_form("ababcabc", "abc")
+        self._verify_result("abc")
+
+    def _submit_form(self, s, t):
         driver = self.driver
-        driver.get("http://localhost:5000")
-        s = driver.find_element_by_name("s")
-        t = driver.find_element_by_name("t")
-        
-        # Example test case
-        s.send_keys("ADOBECODEBANC")
-        t.send_keys("ABC")
-        t.send_keys(Keys.RETURN)
-        
-        result = driver.find_element_by_tag_name("p").text
-        self.assertIn("Result: BANC", result)
+        s_input = driver.find_element(By.NAME, "s")
+        t_input = driver.find_element(By.NAME, "t")
+        s_input.clear()
+        t_input.clear()
+        s_input.send_keys(s)
+        t_input.send_keys(t)
+        t_input.send_keys(Keys.RETURN)
+
+    def _verify_result(self, expected_result):
+        result = self.driver.find_element(By.TAG_NAME, "p").text
+        self.assertIn(f"Result: {expected_result}", result)
 
     def tearDown(self):
         self.driver.close()
